0546d632921df1c7ac5c998d0f0e7b50358ed1ed
DenyT17 <dan.topa@wp.pl>
2023-03-11 19:56:56 +0100

    Movies and Serials DataFrame

--- movie.py
+++ movie.py
@@ -1,19 +1,33 @@
 import pandas as pd
 import numpy as np
-import seaborn as sn
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 pd.set_option('display.max_columns', None)
 data = pd.read_csv('movies.csv')
 
 # Sorting value by movie name
 data = data.sort_values(by=["MOVIES"])
-# Drop duplicate+
-data = data.drop_duplicates(subset=["MOVIES"])
+data = data.rename(columns={"MOVIES":"Name",
+                     "YEAR":"Year/s_of_creation",
+                     "GENRE":"Genre",
+                     "RATING":"Rating",
+                     "ONE-LINE":"Description",
+                     "STARS":"Stars",
+                     "VOTES":"Number_of_votes",
+                     })
 # Drop NAN value from YEAR columns
-data.dropna(subset=["YEAR"],inplace=True)
+data.dropna(subset=["Year/s_of_creation","Rating","RunTime"],inplace=True)
 # Creating two dataframe, first contain only serials and second contain only movies
-serial = data.loc[(data['YEAR'].str.contains("–"))].reset_index(drop=True)
-movie = data.loc[(~data["YEAR"].str.contains("–"))].reset_index(drop=True)
-print(serial)
-print(movie)
+serial = data.loc[(data["Year/s_of_creation"].str.contains("–"))].reset_index(drop=True)
+movie = data.loc[(~data["Year/s_of_creation"].str.contains("–"))].reset_index(drop=True)
+
+# Number of episodes
+episodes = pd.DataFrame(serial["Name"].value_counts().reset_index())
+episodes = episodes.rename(columns={"index":"Name",
+                                    "Name":"Number_of_episodes"})
+serial = serial.drop_duplicates(subset="Name")
+serial = serial.merge(episodes,on="Name")
+serial = serial.drop(["Gross",""],axis=1)
+
+
 
