f3faca0ec0b1f03ae399aac7f0654c9841daf7f1
DenyT17 <dan.topa@wp.pl>
2023-03-13 18:51:18 +0100

    company stats

--- functions.py
+++ functions.py
@@ -2,16 +2,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 import numpy as np
+import pandas as pd
 
-def top_plot(data,label,top):
+def rank_plot(data,label,top,bestworst = "best"):
+    if bestworst == "best":
+        data = data.sort_values(by=label,ascending=False)
+        title = f"Top {top} best {label} movies"
+    elif bestworst == "worst":
+        data = data.sort_values(by=label,ascending=True)
+        title = f"Top {top} worst {label} movies"
     color = ["blue","green","red","cyan","magenta","yellow","black","springgreen","yellowgreen","skyblue"]
     performance = np.arange(0,top,1)
     cmap = dict(zip(performance[:top], color[:top]))
     fig, ax = plt.subplots(figsize=(8,5))
-    data = data.sort_values(by=label,ascending=False)
     plt.bar(x=data['name'].head(top),height=data[label].head(top),color=color[:top])
     ax.set_xticklabels('')
-    plt.title(f"Top {top} {label} movies")
+    plt.title(title)
     plt.xlabel("Movie names")
     plt.ylabel(label)
     patches = [Patch(color=v, label=k) for k, v in cmap.items()]
@@ -24,3 +30,14 @@ def top_plot(data,label,top):
                frameon=False)
     plt.tight_layout()
     plt.show()
+
+def company_stats(data,columns):
+    company = pd.DataFrame(data["company"].value_counts()).reset_index()
+    company = company.rename(columns={"index": "name", "company": "films_number"})
+    for column in columns:
+        stat = pd.DataFrame(data[["company",column]].groupby("company").sum()).reset_index()
+        stat = stat.rename(columns={"company": "name"})
+        company = company.merge(stat, on="name")
+        company[f"mean_{column}"] = company[column] / company["films_number"]
+    company = company.drop(axis=1,columns=["score","relative_revenue"])
+    return company
--- movie.py
+++ movie.py
@@ -1,21 +1,49 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-
-from functions import top_plot
+from functions import rank_plot,company_stats
 
 pd.set_option('display.max_columns', None)
 data = pd.read_csv("movies.csv")
+
 
 # Dropping or filling nan value.
 data["score"].fillna(value=data["score"].mean(),inplace=True)
 data["runtime"].fillna(value=data["runtime"].mean(),inplace=True)
 data["score"].fillna(value=data["gross"].mean(),inplace=True)
 data["votes"].fillna(value=data["votes"].mean(),inplace=True)
-data["gross"].fillna(value=data["gross"].mean(),inplace=True)
-data["budget"].fillna(value=data["budget"].mean(),inplace=True)
 data["rating"].fillna(value="Unknown",inplace=True)
-data = data.dropna(subset=["genre","writer","country","star","country","company"])
+data = data.dropna(subset=["genre","writer","country","star","country","company","budget","gross"])
 
-# Data Visualisation
-top_plot(data,"budget",10)
-top_plot(data,"score",5)
-top_plot(data,"gross",3)
+
+# Creating new columns / dataframe
+genre = pd.DataFrame(data['genre'].value_counts())
+other_gener = genre.loc[genre["genre"] < 300].sum().values
+genre = genre.drop(genre[genre["genre"] < 300].index).reset_index()
+genre.loc[len(genre.index)] = ["Other", other_gener[0]]
+
+data["relative_revenue"] = data["gross"] / data["budget"]
+
+company = company_stats(data,["gross","budget","score","runtime","relative_revenue"])
+
+## Data Visualisation
+
+rank_plot(data,"budget",10)
+rank_plot(data,"score",5)
+rank_plot(data,"gross",10)
+rank_plot(data,"relative_revenue",10)
+
+rank_plot(data,"budget",10,"worst")
+rank_plot(data,"score",5,"worst")
+rank_plot(data,"gross",10,"worst")
+rank_plot(data,"relative_revenue",10,"worst")
+
+rank_plot(company,"budget",10)
+rank_plot(company,"mean_score",5)
+rank_plot(company,"gross",10)
+rank_plot(company,"mean_relative_revenue",10)
+
+fig, ax = plt.subplots(figsize=(8, 5))
+plt.pie(genre["genre"],labels=genre["index"],autopct='%1.0f%%')
+plt.title("The most popular genres")
+plt.show()
+
